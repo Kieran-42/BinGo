54a2ed8e04a642ab1a06c5612a2ecffaaa60a4f9
Westy05 <heronoob21@gmail.com>
2025-02-15 21:16:27 -0800

    Completed getInstructions function

--- screens/SummaryScreen.js
+++ screens/SummaryScreen.js
@@ -11,7 +11,7 @@ const getInstructions = async (wasteType) => {
 	
 	switch (wasteType) {
 		case "battery":
-			solutionObj.locations = [""];
+			solutionObj.locations = ["Battery Retailers"];
 			solutionObj.instructionBulletins = ["Never Dispose of Batteries in Regular Trash: Batteries contain hazardous materials and should not be thrown away with household waste.",
 				"Store Batteries Safely Before Recycling: Keep used batteries in a cool, dry place and avoid storing them in bulk to prevent potential leaks or short-circuiting.",
 				"Use Designated Recycling Programs: Many communities offer battery recycling services or hazardous waste collection events. You can look up for local facilities that can process batteries."
@@ -32,7 +32,7 @@ const getInstructions = async (wasteType) => {
 		case "brown-glass":
 		case "green-glass":
 		case "white-glass":
-			solutionObj.locations = ["Recycling Bin", "Curbside Recycling Bin"];
+			solutionObj.locations = ["Recycling Bins"];
 			solutionObj.instructionBulletins = ["Handling Broken Glass: If the glass is broken, protect your hands from cuts by wearing heavy duty gloves.",
 				"Recyclable Glass: Only glass bottles and jars can be recycled with curbside recycling.",
 				"Rinse Containers: Ensure all glass bottles and jars are empty and rinsed to remove food residues, which can contaminate other recyclables.",
@@ -42,7 +42,7 @@ const getInstructions = async (wasteType) => {
 			break;
 
 		case "cardboard":
-			solutionObj.locations = ["Recycling Bin", "Curbside Recycling Bin"];
+			solutionObj.locations = ["Recycling Bins"];
 			solutionObj.instructionBulletins = ["Keep It Clean and Dry: Ensure all cardboard is free from food, liquid, and other residues. Contaminated cardboard can compromise the recycling process and may end up in landfills instead.",
 				"Flatten Boxes: Break down cardboard boxes before placing them in the recycling bin. Flattening boxes saves space, making collection and processing more efficient.",
 				"Remove Non-Cardboard Materials: Take out any packing materials such as bubble wrap, air pillows, or styrofoam inserts. These items are not recyclable with cardboard and should be disposed of separately."
@@ -50,7 +50,7 @@ const getInstructions = async (wasteType) => {
 			break;
 
 		case "clothes":
-			solutionObj.locations = ["Charities", "Good Will"];
+			solutionObj.locations = ["Charities", "Specific Retailers"];
 			solutionObj.instructionBulletins = ["Donate Clothes: Clothes in good condition can be donated to charities or shelters, extending their lifespan and helping those in need.",
 				"Repurpose Worn-Out Textiles: Old or damaged clothing can be repurposed as cleaning rags or craft materials.",
 				"Utilize Retailer Programs: Some retailers offer take-back programs for textiles, accepting used clothing for recycling or repurposing."
@@ -58,7 +58,7 @@ const getInstructions = async (wasteType) => {
 			break;
 			
 		case "metal":
-			solutionObj.locations = ["Recycling Bin"];
+			solutionObj.locations = ["Recycling Bins"];
 			solutionObj.instructionBulletins = ["Recycle Aluminum and Steel Cans: Empty, dry beverage and food cans are highly recyclable and can be placed in most curbside bins.",
 				"Clean Metal Properly: Remove any food or residues from metals to ensure they are ready for recycling.",
 				"Avoid Including Hazardous Metal Items: Items like propane tanks or electronics (e-waste) require special handling and should not be placed in regular recycling bins. You can look up online for nearby facilities you can take them to."
@@ -67,7 +67,7 @@ const getInstructions = async (wasteType) => {
 			break;
 			
 		case "paper":
-			solutionObj.locations = [""];
+			solutionObj.locations = ["Recycle Bins"];
 			solutionObj.instructionBulletins = ["Recycle Clean Paper Products: Items such as office paper, newspapers, magazines, and envelopes are widely accepted in recycling programs. Ensure they are free from contaminants like food residue or grease before placing them in the recycling bin.",
 				"Avoid Recycling Contaminated or Coated Paper: Paper products that are soiled with food, grease, or coated with materials like wax, plastic, or foil (e.g., certain wrapping papers, laminated documents) are typically non-recyclable and should be disposed of in regular trash."
 			];
@@ -75,7 +75,7 @@ const getInstructions = async (wasteType) => {
 			break;
 
 		case "plastic":
-			solutionObj.locations = [""];
+			solutionObj.locations = ["Recycle Bins"];
 			solutionObj.instructionBulletins = ["Identify Recyclable Plastics: Plastics labeled with recycling codes #1 (PET) and #2 (HDPE) are widely accepted in recycling programs. Check the bottom (or sides) of containers for these codes to determine recyclability.",
 				"Clean and Dry Before Recycling: Ensure plastic items are free from food residues and liquids. Contaminants can hinder the recycling process and may cause entire batches to be discarded.",
 				"Avoid Bagging Recyclables: Place plastics directly into the recycling bin without bagging them, as plastic bags can interfere with sorting machinery. Instead, many retailers offer collection bins for plastic bags, wraps, and films, which are not typically accepted in curbside programs"
@@ -83,7 +83,7 @@ const getInstructions = async (wasteType) => {
 			break;
 
 		case "shoes":
-			solutionObj.locations = [""];
+			solutionObj.locations = ["Charities", "Specific Retailers"];
 			solutionObj.instructionBulletins = ["Donate Usable Shoes: Gently worn shoes can be donated to organizations that distribute them to those in need.",
 				"Explore Retailer Take-Back Programs: Some brands offer programs to recycle old shoes, turning them into new products or materials. You can easily find local ones online.",
 				"Repurpose Worn-Out Footwear: Old shoes can be used for gardening or other activities where new shoes aren't necessary, extending their use before disposal."
@@ -91,11 +91,21 @@ const getInstructions = async (wasteType) => {
 			break;
 
 		case "trash":
-			solutionObj.locations = [""];
-			solutionObj.instructionBulletins = [""];
+			solutionObj.locations = ["Landfill Bin", "Trash Can"];
+			solutionObj.instructionBulletins = ["Secure Loose Materials: Bag items like ashes, sawdust, and pet waste before placing them in the trash to prevent litter during collection.",
+				"Avoid Disposing of Hazardous Materials in Regular Trash: Items like batteries, paints, and chemicals require special handling. Look up your local hazardous waste disposal program for guidance."
+			];
 			break;
 
 		default:
+			solutionObj.locations = ["Unknown"];
+			solutionObj.instructionBulletins = ["Check for Recycling Symbols: Examine product packaging for recycling symbols and codes, which indicate whether an item is recyclable.",
+				"Plastics: Marked with numbers (1-7) inside a triangle, denoting the type of plastic and its recyclability.",
+				"Glass: A triangle of arrows or a glass icon indicates recyclability. Ensure glass is clean and free from contaminants.",
+				"Paper and Cardboard: Symbols like a recycling triangle with a paper icon denote recyclable paper products. Avoid recycling soiled or greasy paper.",
+				"Metals: Aluminum and steel cans often have recycling symbols indicating they are recyclable. Rinse them before recycling.",
+				"Consult Local Guidelines: Waste disposal regulations can vary by location. Refer to your local waste management resources or municipal guidelines to understand specific disposal requirements in your area."
+			];
 			break;
 		}
 
