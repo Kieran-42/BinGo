f807964b09a926c8535db42fae666fd2089eacff
ItsKaiwenDu <TheKaiwenDu@gmail.com>
2025-02-15 16:15:19 -0800

    Commit Frontend
    1. Updated app.json
    2. Reverted package.json
    3. Reverted SummaryScreen.js

--- screens/SummaryScreen.js
+++ screens/SummaryScreen.js
@@ -1,46 +1,28 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, Image, StyleSheet } from "react-native";
-// Import TensorFlow.js and its React Native integration
-import * as tf from "@tensorflow/tfjs";
-import "@tensorflow/tfjs-react-native";
-import { decodeJpeg } from "@tensorflow/tfjs-react-native";
-import * as FileSystem from "expo-file-system";
 
+// Import TensorFlow.js and its React Native integration
+
+import * as FileSystem from "expo-file-system";
 export default function SummaryScreen({ route }) {
   // this is the uri that we passed from the camera screen page
   const { photoUri } = route.params || {};
-
   // This is just randomly selected right now --> we will have to feed image into
   // model later to get this values
   const [classification, setClassification] = useState(null);
   // State variable to hold the loaded Keras model instance
-  const [model, setModel] = useState(null);
 
-  // Load the Keras model when the component mounts
-  useEffect(() => {
-    async function loadModel() {
-      try {
-        // Wait for tfjs to be ready
-        await tf.ready();
-        // we load the Keras model using tf.loadLayersModel.
-        const modelFile = require("src/bin/garbage_classifier_mobilenetv2.keras");
-        const loadedModel = await tf.loadLayersModel(modelFile);
-        console.log("Model loaded:", loadedModel);
-        setModel(loadedModel);
-      } catch (error) {
-        console.log("Error loading model:", error);
-      }
-    }
-    loadModel();
-  }, []);
-
-	return(
-		<View>
-			<Text style={styles.container}>Results</Text>
-			{photoUri && <Image source={{ uri: photoUri }} style={styles.previewImage} />}
-			{classification && (<Text style={styles.classificationText}>Object Classification: {classification} Bin</Text>)}
-		</View>
-	)
+  return (
+    <View>
+      <Text style={styles.container}>Classification Results</Text>
+      {photoUri && <Image source={{ uri: photoUri }} style={styles.previewImage} />}
+      {classification && (
+        <Text style={styles.classificationText}>
+          This item needs to go to the: {classification} bin
+        </Text>
+      )}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
